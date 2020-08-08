03dd54672c57ac8e3fae0d0a2f918b2d78320fe9
Jagadeesh Mangipudi <jagadeesh.mangipudi412@gmail.com>
2020-08-08 18:59:59 +0530

    Commit changes

--- src/p1/Test2.java
+++ src/p1/Test2.java
@@ -2,9 +2,10 @@ package p1;
 
 public class Test2 {
 public static void main(String[] agrs) {
-	System.out.println("Learning GIT commands");
-	System.out.println("Practice GIT");
-	System.out.println("New line in Branch A");
-	System.out.println("New line in main");
+	
+	
+	System.out.println("New line in Branch B1");
+	System.out.println("Modified Code of Branch B1");
+	System.out.println("Modified Code of Branch B1");
 }
 }
